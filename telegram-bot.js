b379d2d00b650cb8c3b2406f2debc4d1f17e7b59
Fernando Iguibejeres <fernando.iguibejeres@mercadolibre.com>
2016-12-14 12:49:36 -0300

    Modularize telegram-bot and movie-fetcher

--- telegram-bot.js
+++ telegram-bot.js
@@ -1,10 +1,11 @@
 let TelegramBot = require('node-telegram-bot-api');
-let getTitle = require('./index');
+let MovieFetcher = require("./movie-fetcher");
+let movieFetcher = new MovieFetcher();
 
 let bot = new TelegramBot('311477110:AAEPYL1lz75Gh52NgJfVbhwYbNnR56rqtIM', { polling: true });
 
 bot.onText(/^\/movie$/, function (msg) {
-    getTitle((titleResult) => {bot.sendMessage(msg.chat.id, titleResult);})
+    movieFetcher.getTitle((titleResult) => {bot.sendMessage(msg.chat.id, titleResult);})
 });
 
 bot.onText(/\/echo (.+)/, function (msg, match) {
