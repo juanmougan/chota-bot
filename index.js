6e0d50644d3756cceee512a500038ab7b91780c4
figui <fer36@hotmail.com>
2016-12-13 23:26:44 -0300

    Config restify to use Openshift environments variables to define IP and Port

--- index.js
+++ index.js
@@ -53,6 +53,9 @@ server.get("/movie", (req, res, next) => {
 	getTitle(res)
 });
 
-server.listen(process.env.PORT || 8080, function() {
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+
+server.listen(server_port, server_ip_address, function() {
 	console.log('%s listening at %s', server.name, server.url);
 });
