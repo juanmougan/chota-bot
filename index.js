cff7663ef64ef72e93e298fa99ee29c53d026a14
Fernando Iguibejeres <fernando.iguibejeres@mercadolibre.com>
2016-12-14 10:16:11 -0300

    Changes for deploy in heroku

--- index.js
+++ index.js
@@ -53,9 +53,6 @@ server.get("/movie", (req, res, next) => {
 	getTitle(res)
 });
 
-var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
-var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
-
-server.listen(server_port, server_ip_address, function() {
+server.listen(process.env.PORT || 8080, function() {
 	console.log('%s listening at %s', server.name, server.url);
 });
